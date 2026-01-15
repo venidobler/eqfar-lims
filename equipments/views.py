b5f65c9b6563ec1e185527fca4862bbe60420275
Venícius <veniciusdobler840@gmail.com>
2026-01-15 15:40:55 -0300

    feat(equipments): add search filter by name, model and brand

--- equipments/views.py
+++ equipments/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404 # <--- Importe o 404
+from django.db.models import Q  # <--- IMPORTANTE: Permite buscas com "OU" (OR)
 from .models import Equipment
 
 def list_equipments(request):
-    # ... (código que já estava aqui) ...
-    equipments = Equipment.objects.all().order_by('name')
-    context = {'equipments': equipments}
+    query = request.GET.get('q', '')
+    equipments = Equipment.objects.all()
+    
+    if query:
+        equipments = equipments.filter(
+            Q(name__icontains=query) |   # Busca no Nome
+            Q(model__icontains=query) |  # Busca no Modelo
+            Q(brand__icontains=query)    # Busca na Marca
+        )
+
+    context = {
+        'equipments': equipments,
+        'query': query
+    }
     return render(request, 'equipments/list.html', context)
 
 # --- ADICIONE ESTA NOVA FUNÇÃO ABAIXO ---
